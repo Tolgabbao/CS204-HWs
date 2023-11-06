9b895e382923287d440cedc89aa596737719dd3a
Tolga <ttoker10@gmail.com>
2023-11-06 15:49:50 +0300

    Refactor code to implement workload processing and service execution

--- HW#3/main.cpp
+++ HW#3/main.cpp
@@ -6,42 +6,122 @@
 #include <string>
 #include <vector>
 #include <sstream>      // std::stringstream
+
+#include "dataStructures.h"
+
 using namespace std;
 //TODO: Modify this part to make sure to impelement the logic for 10 requests for instructors and 1 request for students
-void processWorkload()
-{
-	if (!instructorsQueue.isEmpty())
-	{
-		cout<<"Processing instructors queue..."<<endl;
-		
-		cout<<"Processing "<<"JOB NAME"<<"'s request (with ID "<<"JOB ID"<<") of service (function):\n"<<"FUNCTION NAME"<endl;
-		//You need to implement the processWorkload --> you can modify inputs
-        //processWorkload(...);
-		
-        cout<<"GOING BACK TO MAIN MENU"<<endl;
-	}
-	else if (!studentsQueue.isEmpty())
-	{
-        //TODO: This should print when you implemented 10 requests for instructors and 1 request for students logic
-		//cout<<"10 instructors are served. Taking 1 student from the queue..."<<endl;
 
-		cout<<"Instructors queue is empty. Proceeding with students queue..."<<endl;
-		cout<<"Processing "<<"JOB NAME"<<"'s request (with ID "<<"JOB ID"<<") of service (function):\n"<<"FUNCTION NAME"<endl;
-		cout<<"-------------------------------------------------------"<<endl;
-        
-        //You need to implement the processWorkload --> you can modify inputs
-		//processWorkload(...);
+void processWorkload(InstructorQueue& instructorsQueue, StudentQueue& studentsQueue, ServiceList& serviceList, PaymentList& paymentList) {
+	string id = "";
+	string name = "";
+    if (!instructorsQueue.isEmpty()) {
+        cout << "Processing instructors queue..." << endl;
+        string request = instructorsQueue.dequeue(id, name);
+        cout << "Processing prof." << name << "'s request (with ID " << id << ") of service (function):" << endl << request << endl;
+		cout << "-------------------------------------------------------" << endl;
+		CommandStack programStack;
+        serviceList.executeService(request,paymentList, programStack, id, name, "instructor");
+        cout << "GOING BACK TO MAIN MENU" << endl;
+    } else if (!studentsQueue.isEmpty()) {
+        cout << "Instructors queue is empty. Proceeding with students queue..." << endl;
+        string request = studentsQueue.dequeue(id, name);
+        cout << "Processing" << name << "'s request (with ID " << id << ") of service (function):" << endl << request << endl;
+		cout << "-------------------------------------------------------" << endl;
+		CommandStack programStack;
+        serviceList.executeService(request, paymentList, programStack, id, name, "student");
+        cout << "GOING BACK TO MAIN MENU" << endl;
+    } else {
+        cout << "Both instructor's and student's queue is empty." << endl << "No request is processed." << endl << "GOING BACK TO MAIN MENU" << endl;
+    }
+}
+
+void addInstructorWorkload(InstructorQueue& instructorsQueue) {
+	string request;
+	string name;
+	string id;
+	cout << "Add a service (function) that the instructor wants to use: ";
+	cin >> request;
+	cout << "Give instructor's name: ";
+	cin >> name;
+	cout << "Give instructor's ID: ";
+	cin >> id;
+	instructorsQueue.enqueue(request, name, id);
+	cout << "Prof. " << name << "'s service request of " << request << "has been put in the instructor's queue." << endl; 
+	cout << "Waiting to be served..." << endl;
+}
+
+void addStudentWorkload(StudentQueue& studentsQueue) {
+	string request;
+	string name;
+	string id;
+	cout << "Add a service (function) that the student wants to use: ";
+	cin >> request;
+	cout << "Give student's name: ";
+	cin >> name;
+	cout << "Give student's ID: ";
+	cin >> id;
+	studentsQueue.enqueue(request, name, id);
+	cout << name << "'s service request of " << request << "has been put in the student's queue." << endl;
+	cout << "Waiting to be served..." << endl;
+}
+
+void displayUsersPayments(PaymentList paymentList) {
+	paymentList.printPayments();
+}
+
+//Initialize the service list (read from the file)
+void init(ServiceList& serviceList){
+	//Initialize the queues
+	//Initialize the service list
+	//Read from the file
+	//Add services to the service list
+	
+	//Read from the file
+	ifstream file;
+	char choice = 'y';
+	while (choice == 'Y' || choice == 'y') {
+		cout << "If you want to open a service (function) defining file," << endl;
+		cout << "then press (Y/y) for 'yes', otherwise press any single key" << endl;
+		cin >> choice;
+		if (choice == 'Y' || choice == 'y') {
+			string fileName;
+			cout << "Enter the input file name: ";
+			cin >> fileName;
+			file.open(fileName);
+			if (!file) {
+				cout << "Unable to open file" << endl;
+				exit(1);
+			}
+		}
 		
-        cout<<"GOING BACK TO MAIN MENU"<<endl;
+		string line;
+		vector<string> commands;
+		string serviceName;
+		while (getline(file, line)) {
+			if (line.find(":") != string::npos) {
+				serviceName = line.substr(0, line.find(":"));
+			} else if (line.find(";") != string::npos) {
+				commands.push_back(line.substr(0, line.find(";")));
+			}
+		}
+		file.close();
+		serviceList.addService(serviceName, commands);
 	}
-	else
-	{
-		cout<<"Both instructor's and student's queue is empty.\nNo request is processed."<<endl<<"GOING BACK TO MAIN MENU"<<endl;
-	}
+	//Add services to the service list
+	cout << "------------------------------------------------------------------" << endl <<
+	"PRINTING AVAILABLE SERVICES (FUNCTIONS) TO BE CHOSEN FROM THE USERS" << endl <<
+	"------------------------------------------------------------------" << endl;
+	serviceList.printServices();
 }
 
 int main()
 {
+	InstructorQueue instructorsQueue(10);
+	StudentQueue studentsQueue;	
+	ServiceList serviceList;
+	PaymentList paymentList;
+	init(serviceList);
 	while (true){		
        cout << endl;
  		cout<<"**********************************************************************"<<endl
@@ -61,16 +141,16 @@ int main()
 				cout<<"PROGRAM EXITING ... "<<endl;
 				exit(0);
 			case 1:
-				addInstructorWorkload();
+				addInstructorWorkload(instructorsQueue);
 				break;
 			case 2:
-				addStudentWorkload();
+				addStudentWorkload(studentsQueue);
 				break;
 			case 3:
-				processWorkload();
+				processWorkload(instructorsQueue, studentsQueue, serviceList, paymentList);
 				break;
 			case 4:
-				displayUsers();
+				displayUsersPayments(paymentList);
 				break;
 			default:
 				cout<<"INVALID OPTION!!! Try again"<<endl;

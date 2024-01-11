f3be57c2f99e2abde6f8e170b56abb92096fb0ac
Tolga <ttoker10@gmail.com>
2024-01-11 11:28:08 +0300

    Final revision

--- HW#6/sketches.cpp
+++ HW#6/sketches.cpp
@@ -1,3 +1,4 @@
+// Tolga Toker -- 32639
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -87,69 +88,7 @@ public:
   virtual bool isMember(string s) = 0;
 };
 
-/*A Bloom Filter is a probabilistic data structure used for testing whether an element is a member of a set or
-not. It is particularly efficient in terms of space when compared to other data structures like hash tables or sets,
-but it comes with a trade-off in terms of accuracy. A Bloom Filter may sometimes produce false positives,
-meaning it might mistakenly claim that an element is in the set when it’s not, but it never produces false
-negatives (if it says an element is not in the set, it is definitely not in the set). For the homework, your Bloom
-Filter class should include a bit array that is made up of uint64 t variables and m independent hash functions.
-Usage of Bloom Filter is as follows;
-Step 1: Initialization
-• Allocate memory for size/64 64-bit unsigned integers where size, i.e., the number of bits the sketch will
-use, is given as a parameter during construction. You need to set all these bits to 0.
-4
-0 0 0 0 0 0 0 0 0
-hash_1("apple") = 1
-0 1 2 3 4 5 6 7 8
-0 1 0 1 0 0 0 1 0 0 1 2 3 4 5 6 7 8
-hash_2("apple") = 7
-hash_3("apple") = 3
-insert("apple")
-bit array
-bit array
-hash_1("ware") = 2
-hash_2("ware") = 6
-hash_3("ware") = 5
-insert("ware")
-0 1 1 1 0 1 1 1 0 0 1 2 3 4 5 6 7 8
-bit array
-hash_1("ware") = 2
-hash_2("ware") = 6
-hash_3("ware") = 5
-isMember("ware")
-true positive 
-hash_1("astral") = 1
-hash_2("astral") = 4
-hash_3("astral") = 8
-isMember("astral")
-hash_1("tuesday") = 5
-hash_2("tuesday") = 1
-hash_3("tuesday") = 2
-isMember("tuesday")
-true negative  false positive 
-5
-• Prepare k = 3 hash functions - use the a, b values above. You can put these pairs in an array to loop over
-them (i.e., to loop over the hash functions).
-Step 2: Insertion
-• To insert an item, acquire k = 3 hash values from independent hash functions (i.e., using different a, b
-pairs).
-• For each hash value, compute the index of the bit to be modified by taking the modulo of the hash value
-w.r.t. size (i.e., the number of bits).
-• Set the corresponding bits of the Bloom Filter (to 1) where the bits’ indices are obtained from the above
-item.
-Step 3: Membership Query
-To determine if an item is previously inserted into the bloom filter, simply act similar to Step 2.
-• To query an item, acquire k = 3 hash values from independent hash functions (i.e., using different a, b
-pairs).
-• For each hash value, compute the index of the bit to be modified by taking the modulo of the hash value
-w.r.t. size (i.e., the number of bits).
-• Check the corresponding bits of Bloom Filter where the bits’ indices are obtained from the above item.
-• If all checked bits are 1 then return true. Note that the answer can be wrong.
-• If any of the checked bits are 0 then return false. This item is definitely not inserted into the filter
-before.
-In the HW, you will use 3 hash functions (check the hash function implementation given above and three
-possible a, b pairs) to test the accuracy of the Bloom Filter sketch with different sizes on membership queries.
-*/
+
 class BloomFilter : public MembershipSketch{
 private:
   int k;
@@ -161,8 +100,8 @@ public:
     this->k = k;
     this->size = size;
     this->_switch = _switch;
-    filter = new uint64_t[size / 64];
-    for(int i = 0; i < size / 64; i++){
+    filter = new uint64_t[size/64];
+    for(int i = 0; i < size/64; i++){
       filter[i] = 0;
     }
   }
@@ -173,29 +112,146 @@ public:
 
   void add(const std::string& s){
     uint64_t hash;
-    if(_switch == 1)
-      hash = ax_b_string_func(a1, b1, s);
-    else
-      hash = murmur_hash(s);
-    for(int i = 0; i < k; i++){
-      int index = hash % size;
-      filter[index / 64] |= (1ULL << (index % 64));
-      hash = hash >> 8;
+    uint64_t hash_1, hash_2, hash_3, hash_4, hash_5;
+    int index_1, index_2, index_3, index_4, index_5;
+    // use k different hash functions from independent hash functions, ie different a, b pairs stated above like a1 b1, a2 b2, a3 b3
+    if(_switch == 1){
+      for (int i = 0; i < k; i++){
+        switch(i){
+        case 0:
+          hash_1 = ax_b_string_func(a1, b1, s);
+          index_1 = hash_1 % size;
+          break;
+        case 1:
+          hash_2 = ax_b_string_func(a2, b2, s);
+          index_2 = hash_2 % size;
+          break;
+        case 2:
+          hash_3 = ax_b_string_func(a3, b3, s);
+          index_3 = hash_3 % size;
+          break;
+        case 3:
+          hash_4 = ax_b_string_func(a4, b4, s);
+          index_4 = hash_4 % size;
+          break;
+        case 4:
+          hash_5 = ax_b_string_func(a5, b5, s);
+          index_5 = hash_5 % size;
+          break;
+        }
+      }
     }
+    else {
+      for (int i = 0; i < k; i++){
+        switch(i){
+        case 0:
+          hash_1 = murmur_hash(s);
+          index_1 = hash_1 % size;
+          break;
+        case 1:
+          hash_2 = murmur_hash(s);
+          index_2 = hash_2 % size;
+          break;
+        case 2:
+          hash_3 = murmur_hash(s);
+          index_3 = hash_3 % size;
+          break;
+        case 3:
+          hash_4 = murmur_hash(s);
+          index_4 = hash_4 % size;
+          break;
+        case 4:
+          hash_5 = murmur_hash(s);
+          index_5 = hash_5 % size;
+          break;
+        }
+      }
+    }
+
+    // set the corresponding bits of the Bloom Filter (to 1) where the bits’ indices are obtained from the above item
+
+    filter[index_1/64] |= (1ULL << (index_1 % 64));
+    filter[index_2/64] |= (1ULL << (index_2 % 64));
+    filter[index_3/64] |= (1ULL << (index_3 % 64));
+    filter[index_4/64] |= (1ULL << (index_4 % 64));
+    filter[index_5/64] |= (1ULL << (index_5 % 64));
+
   }
 
   bool isMember(string s){
     uint64_t hash;
-    if(_switch == 1)
-      hash = ax_b_string_func(a2, b2, s);
-    else
-      hash = murmur_hash(s);
-    for(int i = 0; i < k; i++){
-      int index = hash % size;
-      if((filter[index / 64] & (1ULL << (index % 64))) == 0){
-  return false;
+    uint64_t hash_1, hash_2, hash_3, hash_4, hash_5;
+    int index_1, index_2, index_3, index_4, index_5;
+    // use k different hash functions from independent hash functions, ie different a, b pairs stated above like a1 b1, a2 b2, a3 b3
+    if(_switch == 1){
+      for (int i = 0; i < k; i++){
+        switch(i){
+        case 0:
+          hash_1 = ax_b_string_func(a1, b1, s);
+          index_1 = hash_1 % size;
+          break;
+        case 1:
+          hash_2 = ax_b_string_func(a2, b2, s);
+          index_2 = hash_2 % size;
+          break;
+        case 2:
+          hash_3 = ax_b_string_func(a3, b3, s);
+          index_3 = hash_3 % size;
+          break;
+        case 3:
+          hash_4 = ax_b_string_func(a4, b4, s);
+          index_4 = hash_4 % size;
+          break;
+        case 4:
+          hash_5 = ax_b_string_func(a5, b5, s);
+          index_5 = hash_5 % size;
+          break;
+        }
       }
-      hash = hash >> 8;
+    }
+    else {
+      for (int i = 0; i < k; i++){
+        switch(i){
+        case 0:
+          hash_1 = murmur_hash(s);
+          index_1 = hash_1 % size;
+          break;
+        case 1:
+          hash_2 = murmur_hash(s);
+          index_2 = hash_2 % size;
+          break;
+        case 2:
+          hash_3 = murmur_hash(s);
+          index_3 = hash_3 % size;
+          break;
+        case 3:
+          hash_4 = murmur_hash(s);
+          index_4 = hash_4 % size;
+          break;
+        case 4:
+          hash_5 = murmur_hash(s);
+          index_5 = hash_5 % size;
+          break;
+        }
+      }
+    }
+
+    // check the corresponding bits of Bloom Filter where the bits’ indices are obtained from the above item
+    // if all checked bits are 1 then return true. Note that the answer can be wrong.
+    if((filter[index_1/64] & (1ULL << (index_1 % 64))) == 0){
+      return false;
+    }
+    if((filter[index_2/64] & (1ULL << (index_2 % 64))) == 0){
+      return false;
+    }
+    if((filter[index_3/64] & (1ULL << (index_3 % 64))) == 0){
+      return false;
+    }
+    if((filter[index_4/64] & (1ULL << (index_4 % 64))) == 0){
+      return false;
+    }
+    if((filter[index_5/64] & (1ULL << (index_5 % 64))) == 0){
+      return false;
     }
     return true;
   }
@@ -207,6 +263,7 @@ public:
   int get_k(){
     return k;
   }
+
 };
 
 class CardinalitySketch : public Sketch{
@@ -268,56 +325,28 @@ class HyperLogLog : public CardinalitySketch{
     }
 };
 
-/*-MinVal Sketch
-Let’s go back to the first approach, MinVal: as mentioned before, only storing the first minimum value can yield
-extremely off estimates since this value can be small. However, to reduce the error, we can store the smallest k
-hash values as the following k-MinVal recipe describes:
-Step 1: Initialization
-You start by initializing a k-MinVal sketch which contains an array of uint64 t to store the k minimum hash
-values found so far. Assume that k = 3 (i.e., we keep 3 minimum values)
-[−1, −1, −1]
-(-1 denotes that the corresponding entry is initially empty).
-3
-Step 2: Hashing Data Elements
-For each data element in your stream, apply the hash function and calculate its hash value.
-Example Data Stream: ["apple", "banana", "cherry", "date", "elderberry"]
-Hash Values:
-apple− > 112
-banana− > 90
-cherry− > 150
-date− > 75
-elderberry− > 130
-Step 3: Updating the Minimum Values Array
-After computing each hash, update the array to maintain only the ’k’ smallest unique hash values.
-Add hash of "apple": [112, −1, −1]
-Add hash of "banana": [90, 112, −1]
-Add hash of "cherry": [90, 112, 150]
-Add hash of "date": [75, 90, 112]
-Add hash of "elderberry": [75, 90, 112] (130 is not in the top 3 minimum values).
-Step 4: Estimating the Number of Distinct Elements
-Once all elements have been processed, use the values in the array to estimate the total number of distinct
-elements in the data stream. The formula often used is:
-DistinctCount ≈
-(k − 1) × Maximum possible hash value
-k’th minimum hash value
-• k’th minimum hash value = 112 (last value in the array)
-• Maximum possible hash value: (depends on hash function implementation - you need to use the appropriate value from climits)
-• Distinct Count Estimation: Assuming the maximum possible hash value = 28 − 1 (8-bit unsigned hash
-values are assumed for demonstration)
-DistinctCount = (3 − 1) ∗ (28 − 1)/112 ≈ 4.55
-*/
+
 class KMinVal : public CardinalitySketch{
 private:
   int K;
   int _switch; // select which hash function to use
   uint64_t* vals; // array
 public:
+  // K is the number of minimum values to keep track of
+
+  // step 1: initialize the array to all -1's
+  // step 2: for each element, hash it and find the index of the minimum value
+  // step 3: if the hashed value is smaller than the minimum value, replace it
+
+  // step 4: estimate the cardinality using distinctCount = (k-1) * maximumpossiblevalue / k'th minimum value
+  // maximumpossiblevalue = take it from climits
+
   KMinVal(int K, int _switch){
     this->K = K;
     this->_switch = _switch;
     vals = new uint64_t[K];
     for(int i = 0; i < K; i++){
-      vals[i] = ULLONG_MAX;
+      vals[i] = -1;
     }
   }
 
@@ -331,22 +360,40 @@ public:
       hash = ax_b_string_func(a4, b4, s);
     else
       hash = murmur_hash(s);
+    // after computing the hash, update the array to maintain only the K smallest unique hash values
+    int index = -1;
     for(int i = 0; i < K; i++){
-      if(hash < vals[i]){
-        for(int j = K - 1; j > i; j--){
-          vals[j] = vals[j - 1];
-        }
-        vals[i] = hash;
-        break;
+      if(vals[i] == hash){
+  return;
       }
+      if(vals[i] == -1){
+  index = i;
+  break;
+      }
+    }
+    if(index == -1){
+      int max_index = 0;
+      for(int i = 1; i < K; i++){
+  if(vals[i] > vals[max_index]){
+    max_index = i;
+  }
+      }
+      if(hash < vals[max_index]){
+  vals[max_index] = hash;
+      }
+    }
+    else{
+      vals[index] = hash;
     }
   }
 
+
   double estimateCardinality(){
-    double estimate = (K - 1) * (pow(2, 64) - 1) / vals[K - 1];
+    sort(vals, vals + K);
+    double estimate = (K - 1) * (double) ULLONG_MAX / (double) vals[K-1];
     return estimate;
   }
-
+  
 };
 
 
